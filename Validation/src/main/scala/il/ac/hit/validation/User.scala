b3c41ea6f77cc2ae8899dc8d4763047d3308a938
orelrafa <orelrafailov1998@gmail.com>
2024-10-08 11:11:07 +0300

    Changed validations type from sequence to iterable, never used any of the seq features. Added ScalaDoc documentation throughout the project and additional comments. Changed HaimMain to the Demo code that is present in the final project doc to match his test. Undone the IDE recommended change of the explicit type

--- Validation/src/main/scala/il/ac/hit/validation/User.scala
+++ Validation/src/main/scala/il/ac/hit/validation/User.scala
@@ -1,5 +1,13 @@
 package il.ac.hit.validation
 
+/**
+ * Represents a user with basic account information.
+ *
+ * @param username The username of the user.
+ * @param email The email address of the user.
+ * @param password The password of the user.
+ * @param age The age of the user.
+ */
 class User(
   val username: String, val email: String,
   val password: String, val age: Int) {
--- Validation/src/main/scala/il/ac/hit/validation/Valid.scala
+++ Validation/src/main/scala/il/ac/hit/validation/Valid.scala
@@ -1,6 +1,26 @@
 package il.ac.hit.validation
 
+/**
+ * Represents a successful validation result.
+ *
+ * @param reason The reason for the successful validation.
+ *
+ * This class extends [[ValidationResult]] and overrides `isValid` to return `true`.
+ * It also provides an optional reason for the validation success through the `getReason` method.
+ */
 class Valid(reason: String) extends ValidationResult {
+
+  /**
+   * Indicates that the validation was successful.
+   *
+   * @return `true`, indicating the validation passed.
+   */
   override def isValid: Boolean = true
+
+  /**
+   * Provides the reason for the successful validation.
+   *
+   * @return An `Option` containing the success reason.
+   */
   override def getReason: Option[String] = Some(reason)
 }
